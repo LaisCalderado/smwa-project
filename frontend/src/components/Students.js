55af715406292ea74df61cc0fb3c3919a0e8f760
LaisCalderado <laiscalderaro06@gmail.com>
2023-08-23 23:58:23 -0400

    Voltando sem o bloq de login

--- frontend/src/components/Students.js
+++ frontend/src/components/Students.js
@@ -7,45 +7,45 @@ const Students = () => {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-   let mounted = true;
-   getStudents()
-     .then(data => {
-       if(mounted) {
-         setStudents(data)
-       }
-     })
-   return () => mounted = false;
- }, [])
+    let mounted = true;
+    getStudents()
+      .then(data => {
+        if (mounted) {
+          setStudents(data)
+        }
+      })
+    return () => mounted = false;
+  }, [])
 
-  return(
-   <div className="container-fluid side-container">
-   <div className="row side-row" >
-    <p id="before-table"></p>
+  return (
+    <div className="container-fluid side-container">
+      <div className="row side-row" >
+        <p id="before-table"></p>
         <Table striped bordered hover className="react-bootstrap-table" id="dataTable">
-        <thead>
+          <thead>
             <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Registration No</th>
-            <th>Email</th>
-            <th>Course</th>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Registration No</th>
+              <th>Email</th>
+              <th>Course</th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             {students.map((stu) =>
-            <tr key={stu.id}>
+              <tr key={stu.id}>
                 <td>{stu.studentId}</td>
                 <td>{stu.FirstName}</td>
                 <td>{stu.LastName}</td>
                 <td>{stu.RegistrationNo}</td>
                 <td>{stu.Email}</td>
                 <td>{stu.Course}</td>
-            </tr>)}
-        </tbody>
-    </Table>
+              </tr>)}
+          </tbody>
+        </Table>
+      </div>
     </div>
-  </div>
   );
 };
 
